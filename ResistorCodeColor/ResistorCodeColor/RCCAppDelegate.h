4ad49d759e105300301300b7ddbaf6d221c690b8
Nicolas Senaud <nicolas@senaud.fr>
2013-12-01 01:49:42 +0100

    First Release, with full features operational

--- ResistorCodeColor/ResistorCodeColor/RCCAppDelegate.h
+++ ResistorCodeColor/ResistorCodeColor/RCCAppDelegate.h
@@ -9,7 +9,25 @@
 #import <Cocoa/Cocoa.h>
 
 @interface RCCAppDelegate : NSObject <NSApplicationDelegate>
+{
+	char currentState;
+	NSInteger *resistor;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *resistorDisplay;
+@property (assign) IBOutlet NSLevelIndicator *state;
+
+- (IBAction)black:(id)sender;
+- (IBAction)brown:(id)sender;
+- (IBAction)red:(id)sender;
+- (IBAction)orange:(id)sender;
+- (IBAction)yellow:(id)sender;
+- (IBAction)green:(id)sender;
+- (IBAction)blue:(id)sender;
+- (IBAction)violet:(id)sender;
+- (IBAction)grey:(id)sender;
+- (IBAction)white:(id)sender;
+- (void)newEntry:(int)color withMultiplier:(int)multiplier;
 
 @end
--- ResistorCodeColor/ResistorCodeColor/RCCAppDelegate.m
+++ ResistorCodeColor/ResistorCodeColor/RCCAppDelegate.m
@@ -8,11 +8,117 @@
 
 #import "RCCAppDelegate.h"
 
+#define BLACK  0
+#define BROWN  1
+#define RED    2
+#define ORANGE 3
+#define YELLOW 4
+#define GREEN  5
+#define BLUE   6
+#define VIOLET 7
+#define GREY   8
+#define WHITE  9
+
+#define BLACK_MULTIPLIER  1e0
+#define BROWN_MULTIPLIER  1e1
+#define RED_MULTIPLIER    1e2
+#define ORANGE_MULTIPLIER 1e3
+#define YELLOW_MULTIPLIER 1e4
+#define GREEN_MULTIPLIER  1e5
+#define BLUE_MULTIPLIER   1e6
+#define VIOLET_MULTIPLIER 1e7
+#define GREY_MULTIPLIER   1e8
+#define WHITE_MULTIPLIER  1e9
+
 @implementation RCCAppDelegate
+
+- (id) init
+{
+	self = [super init];
+	if (self)
+	{
+		NSLog(@"init");
+		
+		currentState = 1;
+		[_resistorDisplay setStringValue:@"Type Color Code"];
+	}
+	
+	return self;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
 	// Insert code here to initialize your application
+}
+
+- (void)newEntry:(int)color withMultiplier:(int)multiplier
+{
+	switch (currentState) {
+		case 1:
+			[_resistorDisplay setIntegerValue:color];
+			currentState++;
+			[_state setIntegerValue:currentState];
+			break;
+			
+		case 2:
+			[_resistorDisplay setIntegerValue:(10*[_resistorDisplay integerValue]+color)];
+			currentState++;
+			[_state setIntegerValue:currentState];
+			break;
+			
+		case 3:
+			[_resistorDisplay setIntegerValue:([_resistorDisplay integerValue]*multiplier)];
+			currentState = 1;
+			[_state setIntegerValue:currentState];
+			break;
+			
+		default:
+			NSLog(@"Error: State Unknown");
+			[_resistorDisplay setStringValue:@"Error!"];
+			currentState = 4;
+			[_state setIntegerValue:currentState];
+			break;
+	}
+}
+
+- (IBAction)black:(id)sender {
+	[self newEntry:BLACK withMultiplier:BLACK_MULTIPLIER];
+}
+
+- (IBAction)brown:(id)sender {
+	[self newEntry:BROWN withMultiplier:BROWN_MULTIPLIER];
+}
+
+- (IBAction)red:(id)sender {
+	[self newEntry:RED withMultiplier:RED_MULTIPLIER];
+}
+
+- (IBAction)orange:(id)sender {
+	[self newEntry:ORANGE withMultiplier:ORANGE_MULTIPLIER];
+}
+
+- (IBAction)yellow:(id)sender {
+	[self newEntry:YELLOW withMultiplier:YELLOW_MULTIPLIER];
+}
+
+- (IBAction)green:(id)sender {
+	[self newEntry:GREEN withMultiplier:GREEN_MULTIPLIER];
+}
+
+- (IBAction)blue:(id)sender {
+	[self newEntry:BLUE withMultiplier:BLUE_MULTIPLIER];
+}
+
+- (IBAction)violet:(id)sender {
+	[self newEntry:VIOLET withMultiplier:VIOLET_MULTIPLIER];
+}
+
+- (IBAction)grey:(id)sender {
+	[self newEntry:GREY withMultiplier:GREY_MULTIPLIER];
+}
+
+- (IBAction)white:(id)sender {
+	[self newEntry:WHITE withMultiplier:WHITE_MULTIPLIER];
 }
 
 @end
